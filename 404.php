47c308b16570393c346cef44b758f91cf07bbd14
Imanishi MBA <macfoce@gmail.com>
2015-07-22 21:45:19 +0900

    add 503 page

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 <!--[if lt IE 7]><html lang="ja" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]><html lang="ja" class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]><html lang="ja" class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--><html lang="ja" class="no-js"><!-- InstanceBegin template="/Templates/base.dwt" codeOutsideHTMLIsLocked="false" --><!--<![endif]-->
+<!--[if gt IE 8]><!--><html lang="ja" class="no-js"><!--<![endif]-->
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -25,7 +25,6 @@
 <link rel="stylesheet" type="text/css" href="css/reset.css" />
 <link rel="stylesheet" type="text/css" href="css/share.css" />
 <script type="text/javascript" src="js/jquery.js"></script>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDWCtOgVXQMWCZcgPPgvM1VLiiDuNPl4Sk&amp;sensor=false"></script>
 <script type="text/javascript" src="js/share.js"></script>
 <script type="text/javascript" src="js/analytics.js"></script>
 <!-- InstanceBeginEditable name="head" -->
@@ -33,60 +32,5 @@
 <!-- InstanceEndEditable -->
 </head>
 <body id="top">
-<span id="top_line"></span>
-<div id="wrapper">
-<article>
-<!-- InstanceBeginEditable name="body" -->
-<header id="header">
-<h1>ページが見つかりませんでした | 徳島大学病院 薬剤部 ・ 徳島大学 大学院 ヘルスバイオサイエンス研究部 臨床薬剤学分野　ホームページ</h1><!-- #BeginLibraryItem "/Library/sideMenu.lbi" --><div id="side_menu" class="opening">
-<p id="logo"><a href="index.html"><img src="images/share/logo.png" width="174" height="150" alt="徳島大学病院 薬剤部 ・ 徳島大学 大学院 ヘルスバイオサイエンス研究部 臨床薬剤学分野" /></a></p>
-<p id="logo_s"><a href="index.html"><img src="images/share/logo_s.gif" width="184" height="50" alt="徳島大学病院 薬剤部 ・ 徳島大学 大学院 ヘルスバイオサイエンス研究部 臨床薬剤学分野" /></a></p>
-<nav id="main_nav">
-<a id="btn_sp_menu"><img src="images/share/btn_sp_menu.gif" width="40" height="40" alt="" /></a>
-<ul id="menu_ul">
-<li><span class="menu_icon"><img id="menu_index" src="images/share/menu_icon_off.gif" width="9" height="9" alt="" /></span><a href="index.html">トップ</a></li>
-<li><span class="menu_icon"><img id="menu_news" src="images/share/menu_icon_off.gif" width="9" height="9" alt="" /></span><a href="news/index.html">お知らせ</a></li>
-<li><span class="menu_icon"><img id="menu_staff" src="images/share/menu_icon_off.gif" width="9" height="9" alt="" /></span><a href="staff/index.html">スタッフ紹介</a></li>
-<li><span class="menu_icon"><img id="menu_works" src="images/share/menu_icon_off.gif" width="9" height="9" alt="" /></span><a href="works/index.html">業務</a></li>
-<li><span class="menu_icon"><img id="menu_research" src="images/share/menu_icon_off.gif" width="9" height="9" alt="" /></span><a href="research/index.html">研究</a></li>
-<li><span class="menu_icon"><img id="menu_education" src="images/share/menu_icon_off.gif" width="9" height="9" alt="" /></span><a href="education/index.html">教育</a></li>
-<li><span class="menu_icon"><img id="menu_achievements" src="images/share/menu_icon_off.gif" width="9" height="9" alt="" /></span><a href="achievements/index.html">業績</a></li>
-<!--<li><span class="menu_icon"><img id="menu_for_patient" src="../images/share/menu_icon_off.gif" width="9" height="9" alt="" /></span><a href="../for_patient/index.html">患者さまへ</a></li>-->
-<li id="btn_menu_access"><span class="menu_icon"><img id="menu_access" src="images/share/menu_icon_off.gif" width="9" height="9" alt="" /></span><a href="index.html#access_link">アクセス</a></li>
-</ul>
-<div id="sp_menu_ul">
-<ul>
-<li><a href="index.html">トップ</a></li>
-<li><a href="news/index.html">お知らせ</a></li>
-<li><a href="staff/index.html">スタッフ紹介</a></li>
-<li><a href="works/index.html">業務</a></li>
-<li><a href="research/index.html">研究</a></li>
-<li><a href="education/index.html">教育</a></li>
-<li><a href="achievements/index.html">業績</a></li>
-<!--<li><a href="../for_patient/index.html">患者さまへ</a></li>-->
-<li id="btn_sp_menu_access"><a href="#access_link">アクセス</a></li>
-</ul>
-</div>
-</nav>
-</div><!--/#side_menu-->
-<!-- #EndLibraryItem --></header><!--/#header-->
-<section id="main" >
-
-<section>
-<h2>404 PAGE NOT FOUND</h2>
-<p id="h2_str">ページがみつかりませんでした。<br /><br /><a href="http://square.umin.ac.jp">トップページへ戻る</a></p>
-<br />
-<br />
-</section>
-
-
-</section><!--main-->
-<!-- InstanceEndEditable -->
-<a id="gotop" href="#top"><img src="images/share/arrow.png" alt="GO TOP" width="36" height="36" /></a>
-<footer id="footer">
-<p id="copyright"><small>Copyright &copy; <script type="text/javascript">document.write(new Date().getFullYear());</script>Department of Pharmacy, Tokushima University Hospital All Rights Reserved.</small></p>
-</footer>
-</article>
-</div><!--/#wrapper-->
 </body>
-<!-- InstanceEnd --></html>
+</html>
