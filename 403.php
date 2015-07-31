fa89d4bab48f14edb0486ad2ecf1f90ffbc33479
Imanishi MBA <macfoce@gmail.com>
2015-07-31 19:52:18 +0900

    change viewport <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">

--- 403.php
+++ 403.php
@@ -6,7 +6,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">  
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">  
 <!-- InstanceBeginEditable name="head_top" -->
 <title>maintenance</title>
 <meta name="description" content="" />
